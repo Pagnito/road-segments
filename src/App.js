8f92233e91888bb83939785c3048659016be3f78
pagnito <pagnit@gmail.com>
2018-09-18 10:50:14 -0400

    inProcess

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
 import ArcLayer from './components/arcLayer';
 import {scaleQuantile} from 'd3-scale';
-
 import roads from './assets/pruned_extra_roads.json';
 import testerRoads from './assets/tester.json';
-import mapboxgl from 'mapbox-gl';
+import wholeMap from './assets/wholeMap.json';
+import * as d3 from 'd3';
 import './App.css';
 
 const MAPBOX_TOKEN = "pk.eyJ1IjoicGFnbml0byIsImEiOiJjamxhMnY4a3UwNDVhM3BxaTV1NWM3ZGR6In0.pRhJryLN4D7NxD5ZbC1MbA";
@@ -14,6 +14,9 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state={
+      connections:[],
+      hoveredSeg:{},
+      currAndPrev:[],
       startingPoint: [-71.063,42.558],
       viewport: {
         width: window.innerWidth,
@@ -22,108 +25,131 @@ class App extends Component {
         longitude: -71.063,
         latitude: 42.558,
         zoom:11,
-        maxZoom: 21,
+        maxZoom: 20,
         pitch: 50,
-        bearing: 50,
-        interactive: true,
-        featuresById: {}
-      },
-      hoveredItem:null,
-      hoveredColor: [55, 229, 232,100]
+        bearing: 50,     
+      }     
     }
   }
 
    componentDidMount(){
-     const featuresById = {}
-  roads.features.forEach((road,ind) => {
-   // featuresById[road.id]=road
-      if(ind>5 && ind <1000){
-        road.properties.color="#f48942"
-      } else {
-      road.properties.color='#afd36b'
-      }
+     ///making the float part of the id because mapbox gl needs round numbers
+     //this.map = this.reactMap.getMap();
+       //const promise =  new Promise((resolve, reject)=>{
+          //const error = false;
+            for(var key in wholeMap.connections){
+              for(var id in wholeMap.connections[key]){
+                wholeMap.connections[key][id] = wholeMap.connections[key][id].map(connectId =>{         
+                  return connectId.replace('.','')
+                })
+              }
+              wholeMap.connections[key.replace('.','')] = wholeMap.connections[key];
+              delete wholeMap.connections[key];  
+            
+            }     
+            //console.log(wholeMap.connections)
+            this.setState({connections:wholeMap.connections})
+            
+          wholeMap.map.features.forEach((road,ind) => {  
+              road.properties.color='#afd36b'
+              road.id = road.id.replace('.','')
+              road.geometry.coordinates.map(coords=>{coords.reverse()})
+
+              ///injecting connections obj into properties
+              Object.keys(wholeMap.connections).forEach(connection=>{
+                if(road.id==connection){        
+                  road.properties.connections=wholeMap.connections[connection];
+                }
+              })          
+            })
+            //console.log(wholeMap.map)
+            this.setState({i95Points:wholeMap.map.features})
+          /*
+            if(!error){
+              resolve(wholeMap.map.features)
+            } else {
+              reject('Error: Something wong dummy')
+            }
+         })*/
+       
+   
+  
+    /*
+    promise.then((data)=>{
+      let canvas = this.map.getCanvasContainer()
+     console.log(data)
+      let path = d3.geoPath().projection(d3.geoAlbers())
+      var svg = d3.select(canvas).append("svg")
+      .attr('height',this.state.viewport.height)
+      .attr('width',this.state.viewport.width)
+      svg.selectAll('text').append('text').text('supfoo')
+      svg.selectAll('path')
+    .data(data)
+    .enter()
+    .append('path')
+    .attr('d', path)
+    .attr('stroke-width', 10)
+    .attr('stroke', '#fca820')
     })
-  
-    this.setState({data:roads.features})
-
-
-  
-                   
+        */          
     this.map = this.reactMap.getMap();
-    
-     this.map.on('load', () => {
-       //add the GeoJSON layer here
-       console.log(this.state.data)
-        console.log(this.map)
-       this.map.addLayer({
+    this.map._interactive=true
+    this.map.on('load', () => {
+       //console.log(this.state.data)  
+      this.map.addLayer({
             "id": "segments",
             "type": "line",
             "source": {
                 "type": "geojson",
                 "data": {
                     "type": "FeatureCollection",
-                    "features": this.state.data
-                }
+                    "features": this.state.i95Points
+                 }
                     
-            },
+             },
             "layout": {
                 "line-join": "round",
                 "line-cap": "round"
-            },
+             },
             "paint": {
                 //"line-color": ['get','color'],
                 "line-width": 5,
                 "line-color": ["case",
                 ["boolean", ["feature-state", "hover"], false],
-                '#afd36b',
-                '#6a9bd8'
-            ]
-
-            }
-        })
-
-       
-  
-       
-      })
-   
-   
-    ////////////////////////////////////////////////////////
-    /*const linePoints = [];
-    roads.features.forEach(road => {road.properties.color=[244, 187, 65, 200]})
-    this.setState({segments:roads})
-    roads.features.map((seg,ind1)=>{
-      
-      const segment = seg.geometry.coordinates;
-      const segId = seg.id;
-      
-      segment.map((points,ind2) =>{
-        if(ind2<segment.length-1){
-         
-         linePoints.push(
-            {from:[points[0],points[1]], to:[segment[ind2+1][0],segment[ind2+1][1]], id:segId}
-           )
-          }
-      })
-      
-    })
-    this.setState({testerRoads:linePoints})
-      console.log(linePoints)*/
-   /* testerRoads.features.forEach(road => {road.properties.color=[244, 187, 65, 200]})
-    this.setState({testerRoads:testerRoads})
-    const testerPoints = []
-    const testLines=testerRoads.features[3].geometry.coordinates;
-    testLines.map((road,ind)=>{
-      if(ind<testLines.length-1){
-      testerPoints.push(
-        {from:[road[0],road[1]], to:[testLines[ind+1][0],testLines[ind+1][1]], id:'gig'}
-       )
-      }
-    })
-    this.setState({testerRoads:testerPoints})*/
+                 ['get','color'],
+                 '#fca820'
+                 ]                          
+             }
+        });  
+        this.map.addLayer({
+          "id": "connectionSegs",
+          "type": "line",
+          "source": {
+              "type": "geojson",
+              "data": {
+                  "type": "FeatureCollection",
+                  "features": this.state.i95Points
+               }
+                  
+           },
+          "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+           },
+          "paint": {
+              //"line-color": '#1dc6ad',
+              "line-width": 5,
+              "line-color": ["case",
+              ["boolean", ["feature-state", "connection"], false],
+              ['#1dc6ad'],
+              'rgba(244, 232, 66, 0.0)'
+              ]            
+           }     
+      })    
+     })
     //////////////////////////////////////////////////////////////////
    
-    
+    ///fetching starbucks locations for initial arc layer study. to be deleted
     /* fetch('https://opendata.socrata.com/resource/txu4-fsic.json')
     .then(data=>data.json())
     .then(locations => {
@@ -138,88 +164,74 @@ class App extends Component {
    
   }
 
-  hover=(info)=>{
-  
-  var feature = this.map.queryRenderedFeatures([info.offsetCenter.x,info.offsetCenter.y],'segments');
-  
-    if(feature.length>0){
-      var hoveredSegId = feature[0].id
-      if (hoveredSegId) {     
-        this.map.setFeatureState({source: 'segments', id: hoveredSegId}, { hover: true});
-    } 
-      
-     /* var newData =  this.state.data.map(featureInLoop=>{
-        
-        if(feature[0].id==featureInLoop.id){
-          featureInLoop.properties.color = '#6a9bd8';
-       
-        } else {
-          featureInLoop.properties.color = '#afd36b';
-        }
-        
-        return featureInLoop
-      })
-      //this.setState({data:newData});
-      
+  /*hover=(info)=>{
     
-      this.map.getSource('segments').setData({
-        "type": "FeatureCollection",
-        "features": [newData]                  
-    });
-     */
+    var feature = this.map.queryRenderedFeatures([info.offsetCenter.x,info.offsetCenter.y],'segments');
+    console.log(feature[0])
+      if(feature.length>0){
+          var hoveredSegId = feature[0].id;       
+          this.state.currAndPrev.push(hoveredSegId);
+          this.setState({hoveredSeg:feature[0]});
+            if (hoveredSegId) {          
+                this.map.setFeatureState({source: 'segments', id: hoveredSegId}, { hover: true});
+                var connections = JSON.parse(feature[0].properties.connections)
+             
+                if(connections.continue){
+                 
+                    connections.continue.forEach(connectionId=>{
+                      if(Number(connectionId)!==hoveredSegId){
+                        this.map.setFeatureState({source: 'connectionSegs', id: connectionId}, { connection: true});
+                        //console.log(typeof hoveredSegId)
+                        //console.log(typeof connectionId)
+                       }
+                     })
+                 }
+             } 
+          if(this.state.currAndPrev.length > 2){
+          
+              var currAndPrev = this.state.currAndPrev.slice(-2)
+              this.setState({ currAndPrev: currAndPrev })
+              //console.log(currAndPrev)
+              if(this.state.currAndPrev[0] !== this.state.currAndPrev[1]){
+                this.map.setFeatureState({source: 'segments', id: this.state.currAndPrev[0]}, { hover: false});
+               }
+           }   
+       }
+      if(feature.length==0){   
+        if(Object.keys(this.state.hoveredSeg).length>0){         
+          this.map.setFeatureState({source: 'segments', id: this.state.hoveredSeg.id}, { hover: false});
+          this.setState({hoveredSeg:{}})
+         } 
+       }
+  }*/
 
-    }
-    if(feature.length==0){
-      if(hoveredSegId){
-      this.map.setFeatureState({source: 'segments', id: hoveredSegId}, { hover: false});
-      }
-    }
-  }
+
+  /////////////////////////////////////////
   _onViewportChange(viewport){
-    this.setState({
-      viewport: {...this.state.viewport, ...viewport}
-    });
-  }
-  onHover=(info)=> {
-        
-        this.setState({hoveredItem:info})
-        if(info.picked){
-        var segmentsClone = this.state.segments;
-        var hoveredRoad = this.state.segments.features.map(road=>{   
-          if(info.object.id===road.id){   
-            console.log(road)
-            road.properties.color = this.state.hoveredColor;
-            
-          }  else {
-            road.properties.color = [244, 187, 65, 200];
-          }
-          return road 
-        });
-        segmentsClone.features = hoveredRoad
-        this.setState({segments:segmentsClone})
-      }
-     
+      this.setState({
+        viewport: {...this.state.viewport, ...viewport}
+       });
     }
   
 
     renderToolTip=(segment)=>{
-      if(segment){
-        if(segment.picked){
-        return (
-          <div style={{left: `${segment.x}px`, top: `${segment.y}px`}} className="toolTip">
-            <div><span className="bold">Longitude:</span> {segment.lngLat[0]}</div>
-            <div><span className="bold">Latitude:</span> {segment.lngLat[1]}</div>
-            <div><span className="bold">Id:</span> {segment.object.id}</div>
-            <div><span className="bold">Name:</span> {segment.object.properties.name}</div>
-            <div><span className="bold">Surface:</span> {segment.object.properties.surface}</div>
-            <div><span className="bold">Condition:</span> {segment.object.properties.condition}</div>
-            <div><span className="bold">Lanes:</span> {segment.object.properties.lanes}</div>
-            <div><span className="bold">Width:</span> {segment.object.properties.width}</div>
-          </div>
-        )
-        console.log(segment)
+        if(segment){
+          if(segment.picked){
+          return (
+            <div style={{left: `${segment.x}px`, top: `${segment.y}px`}} className="toolTip">
+              <div><span className="bold">Longitude:</span> {segment.lngLat[0]}</div>
+              <div><span className="bold">Latitude:</span> {segment.lngLat[1]}</div>
+              <div><span className="bold">Id:</span> {segment.object.id}</div>
+              <div><span className="bold">Name:</span> {segment.object.properties.name}</div>
+              <div><span className="bold">Surface:</span> {segment.object.properties.surface}</div>
+              <div><span className="bold">Condition:</span> {segment.object.properties.condition}</div>
+              <div><span className="bold">Lanes:</span> {segment.object.properties.lanes}</div>
+              <div><span className="bold">Width:</span> {segment.object.properties.width}</div>
+            </div>
+          )
+          console.log(segment)
+        }
       }
-    }
     }
   render() {
     return (
